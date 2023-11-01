130ac07c49e5dc46dcf7bfdcde32f8cc8accf7d1
Param Siddharth <contact@paramsid.com>
2023-11-01 23:45:26 +0530

    Initialize project
    Signed-off-by: Param Siddharth <contact@paramsid.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rema's Homepage
+This is the source code for Rema's homepage.
+
+# Made with ❤️ by [Param](https://www.paramsid.com).
---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import './app.css';
+
+import Home from './pages/home';
+
+function App() {
+  return <>
+    <Home />
+  </>;
+}
+
+export default App;
